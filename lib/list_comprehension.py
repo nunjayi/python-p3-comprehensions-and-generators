cc8bd13dbfcebd6f910d0ee83c2ee00bf827fd25
nunjayi <epenyash@gmail.com>
2024-06-05 11:35:28 +0300

    list comprehension with  python

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # return a list of even numbers
+   
+    return [x for x in num_list if x%2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    return [x+"!" for x in sentence_list]
